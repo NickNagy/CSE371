f961b1aae0b5ed9aa7f51b61c1d696f868a7cd0c
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2018-12-06 16:05:45 -0800

    Update top.v

--- top.v
+++ top.v
@@ -1,25 +1,28 @@
-module top (CLOCK_50, CLOCK2_50, SW, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK, GPIO_0,
-		        AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT);
+module audioTop (CLOCK_50, CLOCK2_50, SW, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK, GPIO_0,
+		        AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT, LEDR);
 
-	input CLOCK_50, CLOCK2_50;
-	input [3:0] KEY;
-	input [9:0] SW;
-	output [35:0] GPIO_0;
+	input logic CLOCK_50, CLOCK2_50;
+	input logic [3:0] KEY;
+	input logic [9:0] SW;
+	output logic [9:0] LEDR;
+	output logic [35:0] GPIO_0;
 	// I2C Audio/Video config interface
-	output FPGA_I2C_SCLK;
+	output logic FPGA_I2C_SCLK;
 	inout FPGA_I2C_SDAT;
 	// Audio CODEC
-	output AUD_XCK;
-	input AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK;
-	input AUD_ADCDAT;
-	output AUD_DACDAT;
+	output logic AUD_XCK;
+	input logic AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK;
+	input logic AUD_ADCDAT;
+	output logic AUD_DACDAT;
 	
-	// Local wires.
-	wire read_ready, write_ready, read, write, writeMem, readMem, speedUpRecording, slowDownRecording;
-	wire [23:0] readdata_left, readdata_right;
-	wire [23:0] writedata_left, writedata_right;
-	wire reset = ~KEY[0];
-	wire reverse = SW[0];
+	// Local logics.
+	logic read_ready, write_ready, read, write, writeMem, readMem, speedUpRecording, slowDownRecording;
+	logic [23:0] readdata_left, readdata_right;
+	logic [23:0] writedata_left, writedata_right;
+	logic reset;
+	assign reset = ~KEY[0];
+	logic reverse;
+	assign reverse = SW[0];
 
 	/////////////////////////////////
 	// Your code goes here 
@@ -38,19 +41,30 @@ module top (CLOCK_50, CLOCK2_50, SW, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK,
 	
 	//part3 p3L (.clk(CLOCK_50), .reset(reset), .in(readdata_left), .read_ready(read), .write_ready(write), .out(writedata_left));
 	//part3 p3R (.clk(CLOCK_50), .reset(reset), .in(readdata_right), .read_ready(read), .write_ready(write), .out(writedata_right));
+
+	logic [23:0] initSamplePeriod, maxSamplePeriod, minSamplePeriod, samplePeriod;
+	assign initSamplePeriod = 24'd400;
+	assign maxSamplePeriod = initSamplePeriod << 4;
+	assign minSamplePeriod = initSamplePeriod >> 2;
 	
+	// speeding up the recording corresponds to decreasing the period of the sampling clock signal
+	modPeriod mp (.in(initSamplePeriod), .max(maxSamplePeriod), .min(minSamplePeriod), .increase(slowDownRecording), .decrease(speedUpRecording), .clk(CLOCK_50), .reset(SW[9]), .out(samplePeriod), .LEDR(LEDR));
+	
+	logic rwClk;
+	getNewClock newClk (.CLOCK_50(CLOCK_50), .reset(SW[9] | slowDownRecording | speedUpRecording), .period(samplePeriod), .newClock(rwClk));
+
 	trackMemSignals tracker (.signalIn(~KEY[3]), .clk(CLOCK_50), .reset(SW[9]), .write(writeMem), .read(readMem));
 	
 	assign GPIO_0[0] = readMem;
 	assign GPIO_0[1] = writeMem;
 	
-	audioLooper loopLeft (.in(readdata_left), .clk(CLOCK_50), .reset(SW[9]), .write(writeMem), .read(readMem), .speedUpRecording(speedUpRecording), .slowDownRecording(slowDownRecording), .reverse(reverse), .out(writedata_left));
-	audioLooper loopRight (.in(readdata_right), .clk(CLOCK_50), .reset(SW[9]), .write(writeMem), .read(readMem), .speedUpRecording(speedUpRecording), .slowDownRecording(slowDownRecording), .reverse(reverse), .out(writedata_right));
+	audioLooper loopLeft (.in(readdata_left), .clk(CLOCK_50), .reset(SW[9]), .rwClk(rwClk), .write(writeMem), .read(readMem), .reverse(reverse), .out(writedata_left));
+	audioLooper loopRight (.in(readdata_right), .clk(CLOCK_50), .reset(SW[9]), .rwClk(rwClk), .write(writeMem), .read(readMem), .reverse(reverse), .out(writedata_right));
 	
 /////////////////////////////////////////////////////////////////////////////////
 // Audio CODEC interface. 
 //
-// The interface consists of the following wires:
+// The interface consists of the following logics:
 // read_ready, write_ready - CODEC ready for read/write operation 
 // readdata_left, readdata_right - left and right channel data from the CODEC
 // read - send data from the CODEC (both channels)
